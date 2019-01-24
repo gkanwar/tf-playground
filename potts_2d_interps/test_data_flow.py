18cf55c93de29b96db44c664f83d6b3213a483de
Gurtej Kanwar <gurtej@mit.edu>
2019-01-23 19:03:27 -0500

    Fixes bootstrap resampling in TF potts

--- potts_2d_interps/test_data_flow.py
+++ potts_2d_interps/test_data_flow.py
@@ -18,43 +18,35 @@ def single_bootstrap():
 
 ## Multi-ensemble bootstrap example
 def multi_ens_bootstrap():
-    labels = [1,2,3,4]
+    all_labels = [1,2,3,4]
     Ncfg_tot = 50
-    Ncfg = 1
+    Ncfg = 5
     Lx = 4
     Lt = 32
-    boots = []
-    its = []
-    big_dataset = None
     ps = {}
-    for label in labels:
+    batch_size = 8
+    datasets = []
+    for label in all_labels:
         placeholder = tf.placeholder(tf.int32, (Ncfg_tot, Lx, Lt))
         ps[label] = placeholder
-        dataset = tf.data.Dataset.from_tensor_slices(placeholder)
+        dataset = tf.data.Dataset.from_tensor_slices(
+            (np.full((Ncfg_tot,), label), placeholder))
         dataset = dataset.repeat()
         dataset = dataset.shuffle(buffer_size=Ncfg_tot).batch(Ncfg)
-        it = dataset.make_initializable_iterator()
-        its.append(it)
-        boot = it.get_next()
-        boots.append((label, boot))
-        if big_dataset is None:
-            big_dataset = tf.data.Dataset.from_tensors((label, boot))
-        else:
-            big_dataset = big_dataset.concatenate(
-                tf.data.Dataset.from_tensors((label, boot)))
-    big_dataset = big_dataset.repeat()
-    big_dataset = big_dataset.shuffle(buffer_size=len(labels)).batch(8)
+        datasets.append(dataset)
+    big_dataset = tf.data.experimental.sample_from_datasets(datasets)
+    big_dataset = big_dataset.batch(batch_size)
     big_it = big_dataset.make_initializable_iterator()
-    label_and_ens = big_it.get_next()
+    labels, ensembles = big_it.get_next()
     fake_ensembles = [
-        np.full((Ncfg_tot, Lx, Lt), l) for l in labels]
+        np.random.randint(l*100, size=(Ncfg_tot, Lx, Lt)) for l in all_labels]
     feed_ensembles = {}
-    for i,l in enumerate(labels):
+    for i,l in enumerate(all_labels):
         feed_ensembles[ps[l]] = fake_ensembles[i]
     with tf.Session() as sess:
-        sess.run(list(map(lambda x: x.initializer, its)),
-                 feed_dict = feed_ensembles)
-        sess.run(big_it.initializer)
+        sess.run(big_it.initializer, feed_dict=feed_ensembles)
         for i in range(10):
-            out = sess.run(label_and_ens)
-            print(out[0])
+            out_ls, out_ens = sess.run([labels, ensembles])
+            print(out_ls[:,0])
+            print(list(map(np.mean, out_ens)))
+multi_ens_bootstrap()
